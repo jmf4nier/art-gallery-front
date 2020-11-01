21d927a4fddd1fbce9b720600871a2a6a97f6603
jmf4nier <jmf4nier@gmail.com>
2020-11-01 11:00:22 -0600

    moved fetch in Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,20 +12,24 @@ import {
 } from 'react-router-dom'
 
 const Home = () => {
-  useEffect(() => {
+  const fetchUser = () => {
     fetch('http://localhost:5000/users')
       .then((response) => response.json())
       .then((data) =>
         data.map((user) => {
           SetUser(user)
+          
         })
       )
-  })
+  }
+  useEffect(() => {
+    fetchUser()
+  }, []) //vital to stopping unwanted updates otherwise get runaway requests
 
   const [user, SetUser] = useState({})
   //need to remember to use the correct data-type for state placeholder to avoid errors
   const { first_name, last_name } = user
- 
+
   return (
     <div>
       <h1>{first_name + ' ' + last_name}</h1>
